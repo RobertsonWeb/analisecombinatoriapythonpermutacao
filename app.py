108ae4d13c7d0c0168ba35182fe9e9b601964f7a
Robertson Ebling dos Santos <robertson@ersistemas.info>
2014-09-18 10:17:36 -0300

    habilitar o Debug apenas para desenvolvimento

--- app.py
+++ app.py
@@ -17,5 +17,6 @@ def experimento(qtd_amarelas=None, qtd_vermelhas=None, qtd_azuis=None, qtd_verde
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-	app.debug = True
+	if port == 5000:
+		app.debug = True
 	app.run(host='0.0.0.0', port=port)
